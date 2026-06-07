ac57f37f30dcd6e704394b5aafc336f679464eab
Ralf Beckers <ralf.beckers@hotmail.com>
2026-06-07 18:25:01 +0200

    Update

--- Src/ShellLinkShare/Shortcut.LinkTargetIDList.cs
+++ Src/ShellLinkShare/Shortcut.LinkTargetIDList.cs
@@ -1,6 +1,4 @@
 ﻿using ShellLink.Internal;
-using System.Drawing;
-using System.Xml.Linq;
 
 namespace ShellLink;
 
@@ -11,13 +9,13 @@ public sealed partial class Shortcut
     {
         if (linkFlags.HasFlag(LinkFlags.HasLinkTargetIDList))
         {
-            int idListStart = reader.Position; // not including idListSize
+            int offset = 2; // not including idListSize
+
+            int idListStart = reader.Position; 
             int idListSize = reader.ReadInt16();
 
             ConHelp.StartTag("LinkTargetIDList", idListStart, idListSize);
-            //Console.WriteLine();
-            //Console.WriteLine($"LinkTargetIDList (Start: 0x{idListStart - 2:X}, Size: 0x{idListSize:X})");
-
+            
             while (true)
             {
                 short itemIDSize = reader.ReadInt16();
@@ -28,12 +26,7 @@ public sealed partial class Shortcut
             }
 
             int idListEnd = reader.Position;
-            ConHelp.EndTag("LinkTargetIDList", idListStart, idListSize, idListEnd, - 4);
-            //Console.WriteLine($"LinkTargetIDList End: 0x{idListStart + idListSize:X} == 0x{idListEnd:X}");
-            //if (idListStart + idListSize != idListEnd)
-            //{
-            //    Console.Error.WriteLine($"Error: Invalid IDListSize: 0x{idListSize:X} instead of actual size 0x{idListEnd - idListStart:X}");
-            //}
+            ConHelp.EndTag("LinkTargetIDList", idListStart, idListSize, idListEnd, offset);
         }   
     }
 
